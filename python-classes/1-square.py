0ad1d9b25fbd87a06ce2435675fdb1ecba8f8d60
dsa1r <a>
2026-06-14 16:34:09 +0000

    created a private instance attribute

---
+++ python-classes/1-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Square  Class
+
+this class instiate private attribute
+
+"""
+
+
+class Square:
+
+    def __init__(self, new_size):
+        """__init__
+
+        The __init__ method initializes the size value of the square.
+
+        Attributes:
+        size (int): The size of the square.
+
+        """
+        self.__size = new_size
