b19d33f1db9338185f45af3cdb834eb978357096
dsa1r <danamusaad22@gmail.com>
2026-06-15 14:19:16 +0000

    fix error

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,32 +1,24 @@
 #!/usr/bin/python3
-"""Square Class
-
-A Square Class
-
-"""
+"""Define a square class."""
 
 
 class Square:
+    """Represent a square."""
 
     def __init__(self, size=0):
-        """__init__
+        """Initialize a square with a given size.
 
-        The __init__ method initializes the size value of the square.
-
-        Attributes:
-            size (:obj:`int`, optional): The size of the square.
+        Args:
+            size (int): The size of the square.
 
         Raises:
-            TypeError: If `size` type is not `int`.
-
-            ValueError: If `size` is less than `0`.
-
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0.
         """
-
         if type(size) is not int:
-            raise TypeError('size must be an integer')
+            raise TypeError("size must be an integer")
 
         if size < 0:
-            raise ValueError('size must be >= 0')
+            raise ValueError("size must be >= 0")
 
         self.__size = size
