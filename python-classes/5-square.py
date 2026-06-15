83d4abcff509da920879fe5ba69f591211a9f094
dsa1r <danamusaad22@gmail.com>
2026-06-15 14:57:26 +0000

    fix error

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -32,10 +32,10 @@ class Square:
         self.__size = value
 
     def my_print(self):
-        """prints the square"""
+        """Print the square using # characters."""
         if self.__size == 0:
             print()
             return None
 
         for i in range(self.__size):
-        print("#" * self.__size)
+            print('#' * self.__size)
