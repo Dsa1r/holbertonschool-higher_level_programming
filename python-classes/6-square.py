9985faa60bc2f948ce99d70eb6038461b0b5822f
dsa1r <danamusaad22@gmail.com>
2026-06-15 16:38:34 +0000

    fix error

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -39,15 +39,18 @@ class Square:
     @property
     def position (self) :
         """Get/set the current position of the square."""
-        return self._ position
+        return self.__position
 
     @position.setter
     def position (self, value) :
-        if (not isinstance (value, tuple) or len (value) ! = 2 or not all (isinstance (num, int) for num in value) or not all (num >= 0 for num in value)):
+        if (not isinstance(value, tuple) or
+                len(value) != 2 or
+                not all (isinstance (num, int) for num in value) or
+                not all (num >= 0 for num in value)):
             raise TypeError ("position must be a tuple of 2 positive integers")
-        self._ position = value
+        self.__position = value
 
-    def my print (self):
+    def my_print (self):
         """Print the square with the size and position"""
         if self.__size == 0:
             print()
@@ -55,5 +58,5 @@ class Square:
         [print() for i in range(0, self.__position[1])]
         for i in range (0, self.__size):
             [print(" ", end="") for j in range(0, self.__position[0])]
-            [print("#", end="") for k in range(0, self.__size0)]
+            [print("#", end="") for k in range(0, self.__size)]
             print()
