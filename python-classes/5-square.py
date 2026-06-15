c52ca90485fcc199a6128cd7096a1b6f0c257c39
dsa1r <danamusaad22@gmail.com>
2026-06-15 14:48:17 +0000

    created a file to print the square

---
+++ python-classes/5-square.py
@@ -0,0 +1,38 @@
+class Square:
+    """Represent a square."""
+
+    def __init__(self, size=0):
+        """Initialize a square with a given size."""
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
+
+    def area(self):
+        """Return the area of the square."""
+        return self.__size * self.__size
+
+    @property
+    def size(self):
+        """Retrieve the size of the square."""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Set the size of the square."""
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+
+        if value < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = value
+
+    def my_print(self):
+        """prints the square"""
+        for i in size:
+            for i in size
+            print('#')
