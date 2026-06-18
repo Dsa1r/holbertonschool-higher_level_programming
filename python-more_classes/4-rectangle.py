87e98467d4f9a98a3e2e13efcac88846d8acd751
dsa1r <danamusaad22@gmail.com>
2026-06-18 14:23:06 +0000

    create repr

---
+++ python-more_classes/4-rectangle.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+"""Define a class to comput area and perimeter"""
+
+
+class Rectangle:
+    """Represent a rectangle."""
+
+    def __init__(self, width=0, height=0):
+        """Initialize rectangle."""
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        """Get width."""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Set width."""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Get height."""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Set height."""
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        return self.__height*self.__width
+    def perimeter(self):
+        if self.__height == 0 or self.__width == 0:
+            return 0
+        return (2*self.__height) + (2*self.__width)
+
+    def __str__(self):
+        if self.__height == 0 or self.__width == 0:
+            return ""
+        return "\n".join("#" * self.__width for _ in range(self.__height))
+
+    def __repr__(self):
+        return f"Rectangle({self.width}, {self.height})"
+
