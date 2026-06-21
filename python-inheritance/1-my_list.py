a00e0ae4026a6ae49b384c282d1d6ccf83fc7173
dsa1r <danamusaad22@gmail.com>
2026-06-21 16:30:32 +0000

    a

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Define a module to inhernt and methods"""
+
+
+class myList(list):
+    """Define a class to inhernt to"""
+    def print_sorted(self):
+        """Define a method to sort the list"""
+        if issubclass(MyList, list):
+            print(sorted(self))
