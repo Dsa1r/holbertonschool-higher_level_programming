a7c8a5bacc2659b8e3221a8e0c4397774489cb34
dsa1r <danamusaad22@gmail.com>
2026-06-23 15:01:46 +0000

    a

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Define the module for a function"""
+
+
+def is_same_class(obj, a_class):
+    """Ensure the function if its an instance of a class"""
+    return isinstance(obj, a_class)
