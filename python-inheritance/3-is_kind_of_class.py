6a98e2557c78310ba8ab8ef7b7381e1466571bc7
dsa1r <danamusaad22@gmail.com>
2026-06-23 15:18:51 +0000

    a

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -2,6 +2,6 @@
 """Define the module for a finction"""
 
 
-def is_kind_of_class (obj, a_class):
+def is_kind_of_class(obj, a_class):
     """Defune the function is_kind_of_class"""
-    return isinstance (obj, a_class)
+    return isinstance(obj, a_class)
