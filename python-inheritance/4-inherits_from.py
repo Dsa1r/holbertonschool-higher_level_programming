e023dcb8ee5c1782be23be5281fccf67ab9054dd
dsa1r <danamusaad22@gmail.com>
2026-06-23 15:27:21 +0000

    a

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -4,4 +4,4 @@
 
 def inherits_from(obj, a_class):
     """Define a method to ensure if obj inhernted"""
-    return if(isinstance (obj, a_class) or type(obj) is not a_class)
+    return (isinstance (obj, a_class) or type(obj) is not a_class)
