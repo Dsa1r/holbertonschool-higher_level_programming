b0c1cb261e3a2bf7eb666f9a76055e8a154f5ad8
dsa1r <danamusaad22@gmail.com>
2026-07-25 19:05:46 +0200

    a

---
+++ python-object_relational_mapping/model_city.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+Defines the City class for the cities table using SQLAlchemy ORM.
+"""
+
+from sqlalchemy import Column, Integer, String, ForeignKey
+
+from model_state import Base
+
+
+class City(Base):
+    """
+    Represents a city stored in the cities database table.
+    """
+
+    __tablename__ = "cities"
+
+    id = Column(
+        Integer,
+        primary_key=True,
+        nullable=False,
+        autoincrement=True
+    )
+    name = Column(
+        String(128),
+        nullable=False
+    )
+    state_id = Column(
+        Integer,
+        ForeignKey("states.id"),
+        nullable=False
+    )
